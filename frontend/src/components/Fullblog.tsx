524c4989030a8d58a1690ca2844294ad24d6afb5
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-08-19 16:55:37 +0530

    responsiveness improved

--- frontend/src/components/Fullblog.tsx
+++ frontend/src/components/Fullblog.tsx
@@ -10,27 +10,37 @@ const Fullblog = ({ blog }: { blog: Blog }) => {
     return (
         <div>
             <Appbar />
-            <div className='flex flex-col md:grid grid-cols-12 md:px-20 px-10 mt-9 '>
+            <div className='mb-24 flex flex-col gap-x-20 md:grid grid-cols-12 md:px-12 px-7 '>
                 <div className='col-span-8'>
                     <div className='text-2xl md:text-7xl  font-extrabold '>{blog.title}</div>
                     <div className='text-slate-500 text-sm md:text-lg mt-5 font-bold'>{'Post on 2nd December 2023'}</div>
                     <div className=' text-base md:text-2xl text-gray-500  mt-10 leading-snug font-medium'>{blog.content}</div>
                 </div>
-                <div className='col-span-3 '>
+
+
+                <div className='col-span-4 pr-4 md:mt-0 mt-10'>
                     <div className='text-xl font-bold text-gray-400'>Author</div>
-                    <div className=' grid grid-cols-6  mt-4'>
-                        <Avatar size='big' name={`${blog.author.name || 'Anynomous'}`} />
-                        <div className=' flex flex-col items-start col-span-5   '>
-                            <div className=' mb-2   text-2xl font-bold  text-gray-600'> {`${blog.author.name || 'Anynomous'}`}</div>
-                            <div className='flex  text-slate-400 font-semibold text-wrap'>
-                                <p className=''> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis accusamus consequatur perspiciatis nesciunt commodi, libero impedit vitae alias vero praesentium quo earum omnis voluptate.</p>
+
+
+                    <div className='flex flex-col mt-2'>
+
+
+                        <div className='flex gap-2'>
+                            <Avatar size='small' name={`${blog.author.name || 'Anynomous'}`} />
+                            <div className=' mb-2 text-lg  md:text-2xl font-bold  text-gray-600'>
+                                {`${blog.author.name || 'Anynomous'}`}
                             </div>
                         </div>
+                        <p className='text-base font-normal text-gray-400'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis accusamus consequatur perspiciatis nesciunt commodi, libero impedit vitae alias vero praesentium quo earum omnis voluptate.</p>
                     </div>
+
+
+
+
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
 
