79d314426d3caf6d9c19def64ac85c0fe0b580d1
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-08-21 20:00:27 +0530

    button and teaxtarea updated using shadcn

--- frontend/src/pages/PublishBlog.tsx
+++ frontend/src/pages/PublishBlog.tsx
@@ -5,6 +5,10 @@ import { BACKEND_URL } from '../config'
 import { useNavigate } from 'react-router-dom'
 import { useDate } from '../hooks'
 
+import { Button } from "@/components/ui/button"
+import { Textarea } from '@/components/ui/textarea'
+
+
 const PublishBlog = () => {
     const [title, setTitle] = React.useState('')
     const [content, setContent] = React.useState('')
@@ -16,9 +20,11 @@ const PublishBlog = () => {
             <Appbar />
             <div className='flex flex-col items-center'>
                 <div className=' w-3/4'>
-                    <input type="search" onChange={(e) => setTitle(e.target.value)} id="default-search" className="block w-full p-4 ps-10  text-lg font-semibold text-slate-400 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-gray-200" placeholder="Title" required />
-                    <TextArea onChange={(e) => setContent(e.target.value)} />
-                    <button
+
+                    <input type="search" onChange={(e) => setTitle(e.target.value)} id="default-search" className="block w-full p-4 ps-10  text-lg font-semibold   border border-gray-300 rounded-lg bg-gray-50 focus:outline-gray-200" placeholder="Title" required />
+                    {/* <TextArea onChange={(e) => setContent(e.target.value)} /> */}
+                    <Textarea className='mt-10 bg-gray-50 focus:outline-gray-200 text-lg font-semibold text-slate-400' placeholder='Write your blog here ...' onChange={(e) => setContent(e.target.value)} />
+                    <Button className='mt-8'
                         onClick={async () => {
                             const response = await axios.post(`${BACKEND_URL}/api/h1/blog`, {
                                 title,
@@ -34,9 +40,10 @@ const PublishBlog = () => {
                             navigate(`/blog/${response.data.post.id}`)
                         }
                         }
-                        className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                        Publish post
-                    </button>
+
+                    >Publish post
+                    </Button>
+
                 </div>
             </div>
         </div>
