a673a2e64599f885a8a488f45641f2417adcec0f
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-08-28 19:41:00 +0530

    fallback of userdetails

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -27,7 +27,7 @@ const Appbar = () => {
                         </Button>
                     </Link>
                     {loading ? <AvatarSkeleton size='big' /> :
-                        <Avatar name={userDetails?.name} size='big' />
+                        <Avatar name={userDetails?.name || 'U'} size='big' />
                     }
 
                 </div>
--- frontend/src/pages/userDetails.tsx
+++ frontend/src/pages/userDetails.tsx
@@ -5,6 +5,8 @@ import Blogcards from "@/components/Blogcards"
 import BlogSkeleton from "@/components/BlogSkeleton"
 import { useUserDetails } from "@/hooks"
 
+import { Frown } from 'lucide-react';
+
 
 const UserDetails = () => {
 
@@ -16,9 +18,9 @@ const UserDetails = () => {
             <Appbar />
             <div className="flex  flex-col items-center space-y-4 mx-auto mt-8 ">
                 {loading ? <AvatarSkeleton size='large' /> :
-                    <Avatar name={userDetails?.name} size='large' />
+                    <Avatar name={userDetails?.name || 'U'} size='large' />
                 }
-                <h1 className="text-3xl font-bold">{userDetails?.name} </h1>
+                <h1 className="text-3xl font-bold">{userDetails?.name || 'User'} </h1>
                 <div className="max-w-full ">
                     <div>
                         <p>Email : {userDetails?.email} </p>
@@ -29,22 +31,29 @@ const UserDetails = () => {
                     </div>
                 </div>
                 <div className="w-full flex flex-col items-center space-y-12 mx-auto py-20 ">
-                    <div><p className="text-2xl text-slate-500/40 font-bold">Your Posts</p></div>
+                    <div><p className="text-2xl md:text-4xl text-slate-500 font-bold">Your Posts</p></div>
                     {loading ?
                         <>
                             <BlogSkeleton />
                             <BlogSkeleton />
                             <BlogSkeleton />
                             <BlogSkeleton />
-                        </> :
-                        userDetails?.posts.map(blog => <Blogcards
-                            key={blog.id}
-                            id={blog.id}
-                            authorName={userDetails.name || "Anonymous"}
-                            title={blog.title}
-                            content={blog.content}
-                            publishedDate={blog.date ? blog.date : "No Date"}
-                        />)
+                        </> : userDetails?.posts.length === 0 ?
+                            <div className="flex items-center pt-32">
+                                <p className="text-2xl text-slate-500/40 block font-bold mr-4">
+                                    No Posts to Show !!
+                                </p>
+                                <span className="block"><Frown className="w-8 h-8 text-slate-500/40  font-bold block" /></span>
+                            </div>
+                            :
+                            userDetails?.posts.map(blog => <Blogcards
+                                key={blog.id}
+                                id={blog.id}
+                                authorName={userDetails.name || "Anonymous"}
+                                title={blog.title}
+                                content={blog.content}
+                                publishedDate={blog.date ? blog.date : "No Date"}
+                            />)
                     }
                 </div>
 
