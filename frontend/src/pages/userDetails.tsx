a686a3068c19c0abb3d8cbf339335dbea36e405a
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-09-15 23:50:39 +0530

    edit profile dialog box and about section added

--- frontend/src/pages/userDetails.tsx
+++ frontend/src/pages/userDetails.tsx
@@ -3,20 +3,82 @@ import Avatar from "@/components/Avatar"
 import AvatarSkeleton from "@/components/AvatarSkeleton"
 import Blogcards from "@/components/Blogcards"
 import BlogSkeleton from "@/components/BlogSkeleton"
+import { Frown } from 'lucide-react';
 import { useUserDetails } from "@/hooks"
 
-import { Frown } from 'lucide-react';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 
 
 const UserDetails = () => {
 
     const { loading, userDetails } = useUserDetails()
 
-    console.log(loading)
+    function DialogDemo() {
+        return (
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button variant="outline">Edit Profile</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>
+                            Make changes to your profile here. Click save when you're done.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="name" className="text-right">
+                                Name
+                            </Label>
+                            <Input
+                                id="name"
+                                defaultValue="Pedro Duarte"
+                                className="col-span-3"
+                            />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="username" className="text-right">
+                                Username
+                            </Label>
+                            <Input
+                                id="username"
+                                defaultValue="@peduarte"
+                                className="col-span-3"
+                            />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        )
+    }
+
+
+
+
+
+
     return (
         <div>
             <Appbar />
-            <div className="flex  flex-col items-center space-y-4 mx-auto mt-8 ">
+            <div className="flex  flex-col items-center space-y-4 mx-auto mt-8 pb-32">
                 {loading ? <AvatarSkeleton size='large' /> :
                     <Avatar name={userDetails?.name || 'U'} size='large' />
                 }
@@ -29,8 +91,12 @@ const UserDetails = () => {
                     <div>
                         <p>Password : {userDetails?.password} </p>
                     </div>
+
+                    <DialogDemo />
+
+
                 </div>
-                <div className="w-full flex flex-col items-center space-y-12 mx-auto py-20 ">
+                {/* <div className="w-full flex flex-col items-center space-y-12 mx-auto py-20 ">
                     <div><p className="text-2xl md:text-4xl text-slate-500 font-bold">Your Posts</p></div>
                     {loading ?
                         <div className="md:w-full md:ml-40">
@@ -56,8 +122,19 @@ const UserDetails = () => {
                             />)
                     }
                 </div>
-
+*/}
             </div>
+            <hr />
+            <Tabs defaultValue="account" className="mx-12 bg-white">
+                <TabsList className="gap-8 mb-[1px]">
+                    <TabsTrigger className="text-black font-semibold text-2xl " value="account">Home</TabsTrigger>
+                    <TabsTrigger className="text-black font-semibold text-2xl" value="password">About</TabsTrigger>
+                </TabsList>
+                <hr />
+                <TabsContent value="account">Your Library</TabsContent>
+                <TabsContent value="password">About You Goes Here.</TabsContent>
+            </Tabs>
+
         </div>
     )
 }
