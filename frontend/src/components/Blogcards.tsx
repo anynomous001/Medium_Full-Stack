45a55086f92418763543cf474cb409af0c380e78
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-11-10 11:10:36 +0530

    responsiveness updated

--- frontend/src/components/Blogcards.tsx
+++ frontend/src/components/Blogcards.tsx
@@ -14,13 +14,13 @@ const Blogcards = ({ authorName, title, content, publishedDate, id }: BlogcardPr
     return (
         <Link to={`/blog/${id}`} className='  flex flex-col w-full md:w-3/4 gap-2 text-wrap py-6 hover:bg-slate-600/10 rounded-sm px-3'>
             <div className='flex flex-col  gap-2 '>
-                <div className='flex items-center gap-6 '>
+                <div className='flex items-center justify-between gap-3 md:gap-6 '>
                     <Avatar name={authorName} size='small' />
-                    <span className='font-bold text-center text-gray-400 flex items-center gap-2'>{authorName} <span>{'.'}</span> {publishedDate}</span>
+                    <span className='font-bold text-center text-gray-400 flex items-center text-xs gap-1 md:gap-2'>{authorName} <span>{'.'}</span> {publishedDate}</span>
                 </div>
-                <div className='font-bold text-3xl'>{title}</div>
-                <div className='font-medium mt-3 text-xl text-slate-500'>{`${content.slice(0, 200)} ...`}</div>
-                <div className='my-4'>{`Reading ${Math.ceil(content.length / 1000)} minutes`}</div>
+                <div className='font-bold text-lg md:text-3xl'>{title}</div>
+                <div className='font-medium mt-1 md:mt-3 text-sm md:text-xl text-wrap w-[90%] text-slate-500'>{`${content.slice(0, 200)} ...`}</div>
+                <div className=' my-2 md:my-4 text-xs'>{`Reading ${Math.ceil(content.length / 1000)} minutes`}</div>
                 <hr />
 
             </div>
